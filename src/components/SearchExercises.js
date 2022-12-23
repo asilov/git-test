9759039c1f87eefacf233fcde6c8271d4844ca95
Asilbek Asilov <asil.california@gmail.com>
2022-12-22 23:40:21 -0800

    Create horizontal scrollbar and write the functionality

--- src/components/SearchExercises.js
+++ src/components/SearchExercises.js
@@ -1,36 +1,46 @@
-import React, {useState, useEffect} from 'react'
+import React, { useState, useEffect } from 'react'
 import { Box, Button, Stack, TextField, Typography } from '@mui/material'
 
 import { exerciseOptions, fetchData } from '../utils/fetchData'
+import HorizontalScrollbar from './HorizontalScrollbar'
 
 const SearchExercises = () => {
-  const [search, setSearch] = useState('')
-  const [exercises, setExercises] = useState([])
+	const [search, setSearch] = useState('')
+	const [exercises, setExercises] = useState([])
+	const [bodyParts, setBodyParts] = useState([])
 
-  useEffect(() => {
-	const fetchExercisesData = async () => {
-		const bodyPartsData = await fetchData(
-			'https://exercisedb.p.rapidapi.com/exercises',
-			exerciseOptions
-		)
+	useEffect(() => {
+		const fetchExercisesData = async () => {
+			const bodyPartsData = await fetchData(
+				'https://exercisedb.p.rapidapi.com/exercises/bodyPartList',
+				exerciseOptions
+			)
+
+			setBodyParts(['all', ...bodyPartsData])
+		}
+
+		fetchExercisesData()
+	}, [])
+
+	const handleSearch = async () => {
+		if (search) {
+			const exercisesData = await fetchData(
+				'https://exercisedb.p.rapidapi.com/exercises',
+				exerciseOptions
+			)
+
+			const searchedExercises = exercisesData.filter(
+				(exercise) =>
+					exercise.name.toLowerCase().includes(search) ||
+					exercise.target.toLowerCase().includes(search) ||
+					exercise.equipment.toLowerCase().includes(search) ||
+					exercise.bodyPart.toLowerCase().includes(search)
+			)
+
+			setSearch('')
+			setExercises(searchedExercises)
+		}
 	}
-  })
-
-  const handleSearch = async () => {
-	if(search) {
-		const exercisesData = await fetchData('https://exercisedb.p.rapidapi.com/exercises', exerciseOptions)
-
-		const searchedExercises = exercisesData.filter(
-			(exercise) => exercise.name.toLowerCase().includes(search) || 
-			exercise.target.toLowerCase().includes(search) || 
-			exercise.equipment.toLowerCase().includes(search) || 
-			exercise.bodyPart.toLowerCase().includes(search)
-		)
-
-		setSearch('')
-		setExercises(searchedExercises)
-	}
-  }
 
 	return (
 		<Stack alignItems='center' mt='37px' justifyContent='center' p='20px'>
@@ -59,19 +69,24 @@ const SearchExercises = () => {
 					placeholder='Search Exercises'
 					type='text'
 				/>
-				<Button className='search-btn'
-          sx={{
-            bgcolor: '#ff2625',
-            color: '#fff',
-            textTransform: 'none',
-            width: {lg: '175px', xs: '80px'},
-            fontSize: { lg: '20px', xs: '14px'},
-            height: '56px',
-            position: 'absolute',
-            right: '0'
-          }}
-          onClick={handleSearch}
-        >Search</Button>
+				<Button
+					className='search-btn'
+					sx={{
+						bgcolor: '#ff2625',
+						color: '#fff',
+						textTransform: 'none',
+						width: { lg: '175px', xs: '80px' },
+						fontSize: { lg: '20px', xs: '14px' },
+						height: '56px',
+						position: 'absolute',
+						right: '0',
+					}}
+					onClick={handleSearch}>
+					Search
+				</Button>
+			</Box>
+			<Box sx={{ position: 'relative', width: '100%', p: '20px' }}>
+					<HorizontalScrollbar data={bodyParts} />
 			</Box>
 		</Stack>
 	)
